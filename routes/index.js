13c0e198e93224ef0ddb049725420f832593b21b
Optykan <syang0525@gmail.com>
2018-05-12 15:54:55 -0400

    dynamically load posts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	fetch(req.protocol+'://'+req.get('host')+'/api/portfolio/')
+		.then(result=>result.json())
+		.then(portfolio=>{
+			res.render('index', { 
+				title: 'Express', 
+				portfolio: portfolio.response
+			});
+		}).catch(err=>{
+			console.error(err)
+		})
 });
 
 module.exports = router;
