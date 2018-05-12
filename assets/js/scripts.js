a2206613a5e8de9ee5dbd002febfb46f5b00036e
Optykan <syang0525@gmail.com>
2018-05-11 22:20:05 -0400

    added get requests

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,32 @@
+function get(url, params){
+	var xhttp = new XMLHttpRequest();
+
+	let queryString = '';
+	if(params){
+		queryString = Object.keys(params).reduce((acc, key, index)=>{
+			return acc + (index ? '&' : '') +  key + '=' + params[key];
+		}, '?');
+	}
+
+	
+
+	xhttp.open("GET", url+queryString, true);
+	xhttp.send();
+
+	return new Promise((resolve, reject)=>{
+		xhttp.onreadystatechange = function(){
+			// console.log(this)
+			if(this.readyState === 4 && 200 <= this.status && this.status < 400){
+				resolve(JSON.parse(this.responseText))
+			} else {
+				// reject(this.statusText)
+			}
+		}
+	})
+}
+
 (function(){
-	//Hello, world!
+	get('http://localhost:3001/api/portfolio')
+		.then(response=>{console.log(response)})
+		.catch(err=>{console.log(response)})
 })()
--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,5 +1,34 @@
-"use strict";
+'use strict';
+
+function get(url, params) {
+	var xhttp = new XMLHttpRequest();
+
+	var queryString = '';
+	if (params) {
+		queryString = Object.keys(params).reduce(function (acc, key, index) {
+			return acc + (index ? '&' : '') + key + '=' + params[key];
+		}, '?');
+	}
+
+	xhttp.open("GET", url + queryString, true);
+	xhttp.send();
+
+	return new Promise(function (resolve, reject) {
+		xhttp.onreadystatechange = function () {
+			// console.log(this)
+			if (this.readyState === 4 && 200 <= this.status && this.status < 400) {
+				resolve(JSON.parse(this.responseText));
+			} else {
+				// reject(this.statusText)
+			}
+		};
+	});
+}
 
 (function () {
-	//Hello, world!
+	get('http://localhost:3001/api/portfolio').then(function (response) {
+		console.log(response);
+	}).catch(function (err) {
+		console.log(response);
+	});
 })();
